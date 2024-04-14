691a7780b6322e45860d545b7f0dc66535a8e92b
Tuan Vu <business.tuanvu@gmail.com>
2024-04-14 07:33:44 -0500

    Update Thinking.js

--- friend/client/src/components/Thinking.js
+++ friend/client/src/components/Thinking.js
@@ -1,7 +1,11 @@
+// Component displaying a thinking message
 const Thinking = () => {
-    return (<div>
-        Coming up with my response for you...
-    </div>)
-}
+    return (
+        <div>
+            {/* Displaying a message to indicate that the response is being generated */}
+            Coming up with my response for you...
+        </div>
+    );
+};
 
-export default Thinking
+export default Thinking;
